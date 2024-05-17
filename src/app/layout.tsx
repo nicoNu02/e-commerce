9666d3729dcb661f00cbbf236387d0763dfa4588
Nicolas Nuñez <nanvii213@gmail.com>
2024-05-16 23:35:19 -0300

    speed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,7 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body className={mont.className}>
+        <SpeedInsights />
         <LayoutDefault>
           {children}
           {modals}

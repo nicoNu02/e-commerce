77a7fb9b2c314f68841f32946bb9331ac3818389
Nicolas Nuñez <nanvii213@gmail.com>
2024-05-18 23:58:51 -0300

    fixed db

--- src/app/api/test/page.tsx
+++ src/app/api/test/page.tsx
@@ -1,16 +1,1 @@
-import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
-
-export default async function prismaExample() {
-  const newUser = await prisma.user.create({
-    data: {
-      name: "Elliott",
-      email: "xelliottx@example-user.com",
-      image: "asdasd",
-    },
-  });
-
-  const users = await prisma.user.findMany();
-  return <>{users}</>;
-}

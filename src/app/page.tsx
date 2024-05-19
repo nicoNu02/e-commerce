a4f685e919ec16b4ee72426e02ae41fde97013b8
Nicolas Nuñez <nanvii213@gmail.com>
2024-05-19 00:24:51 -0300

    finnaly added db!!!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,11 @@ import SwiperDefault from "./_swipers/swiperDefault";
 import Loading from "./components/loading";
 import Header from "./components/Header";
 import Shipping from "./components/Shipping";
+import prismaExample from "./api/db";
 
-export default function Home() {
+export default async function Home() {
+  const users = await prismaExample();
+  console.log(users);
   return (
     <Suspense fallback={<Loading />}>
       <Header />

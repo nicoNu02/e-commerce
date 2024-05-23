66a48bc042b5d01003fea8b3e8effd36769b8df8
Nicolas Nuñez <nanvii213@gmail.com>
2024-05-22 22:40:34 -0300

    fix

--- src/app/api/auth/image/upload/route.ts
+++ src/app/api/auth/image/upload/route.ts
@@ -1,22 +1,28 @@
 import { put } from "@vercel/blob";
 import { NextResponse } from "next/server";
 
-export async function POST(request: Request): Promise<NextResponse> {
+export async function POST(
+  request: Request
+): Promise<NextResponse | undefined> {
   const { searchParams } = new URL(request.url);
   const filename = searchParams.get("filename");
   console.log(filename);
-  // ⚠️ The below code is for App Router Route Handlers only
-  const blob = await put(filename, request.body, {
-    access: "public",
-    token: "vercel_blob_rw_u8V8yhE8bP8fKg64_9RrmTmPD9wTDw52PzBaIQskfy5u38W",
-  });
-  console.log(blob);
-  // Here's the code for Pages API Routes:
-  // const blob = await put(filename, request, {
-  //   access: 'public',
-  // });
+  if (filename) {
+    // ⚠️ The below code is for App Router Route Handlers only
+    // @ts-ignore
+    const blob = await put(filename, request.body, {
+      access: "public",
+      token: "vercel_blob_rw_u8V8yhE8bP8fKg64_9RrmTmPD9wTDw52PzBaIQskfy5u38W",
+    });
+    console.log(blob);
+    // Here's the code for Pages API Routes:
+    // const blob = await put(filename, request, {
+    //   access: 'public',
+    // });
 
-  return NextResponse.json(blob);
+    return NextResponse.json(blob);
+  }
+  return undefined;
 }
 
 // The next lines are required for Pages API Routes only

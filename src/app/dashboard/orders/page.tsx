e47618c0b4a907260f3ad89463e1d2209ba4f05e
Nicolas Nuñez <nanvii213@gmail.com>
2024-06-12 01:26:34 -0300

    fix dashboard

--- src/app/dashboard/orders/page.tsx
+++ src/app/dashboard/orders/page.tsx
@@ -1,12 +1,16 @@
 import Header from "@/app/components/Header";
 import SearchClient from "@/app/components/SearchClient";
+import Loading from "@/app/components/loading";
+import { Suspense } from "react";
 
 export default function Orders() {
   return (
-    <article className="h-full">
-      <Header />
-      <h1 className="text-2xl font-bold p-4">Pedidos</h1>
-      <SearchClient />
-    </article>
+    <Suspense fallback={<Loading />}>
+      <article className="h-full">
+        <Header />
+        <h1 className="text-2xl font-bold p-4">Pedidos</h1>
+        <SearchClient />
+      </article>
+    </Suspense>
   );
 }
--- src/app/dashboard/products/page.tsx
+++ src/app/dashboard/products/page.tsx
@@ -1,18 +1,21 @@
 import CreateProduct from "@/app/components/CreateProduct";
 import Header from "@/app/components/Header";
 import SearchProduct from "@/app/components/SearchProduct";
-import Image from "next/image";
+import Loading from "@/app/components/loading";
+import { Suspense } from "react";
 
 export default function Products() {
   return (
-    <article className="flex flex-col">
-      <Header />
-      <div className="p-8">
-        <h1 className="text-2xl font-bold">Productos</h1>
-        <SearchProduct />
-        <h1 className="text-2xl font-bold">Crear Producto</h1>
-        <CreateProduct />
-      </div>
-    </article>
+    <Suspense fallback={<Loading />}>
+      <article className="flex flex-col">
+        <Header />
+        <div className="p-8">
+          <h1 className="text-2xl font-bold">Productos</h1>
+          <SearchProduct />
+          <h1 className="text-2xl font-bold">Crear Producto</h1>
+          <CreateProduct />
+        </div>
+      </article>
+    </Suspense>
   );
 }

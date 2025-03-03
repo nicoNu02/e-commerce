5cf3d2792b0be9adbbe8f7136a86ff236e4269bf
Nicolas Nuñez <nicolas.nunez@devlabs.dev>
2025-03-03 20:40:18 -0300

    fix axios

--- src/axios/index.ts
+++ src/axios/index.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-export const baseURL = `${process.env.NEXT_PUBLIC_URL}api/auth`;
+export const baseURL = `${process.env.NEXT_PUBLIC_API_URL}api/auth`;
 
 export const apiService = axios.create({
   baseURL,
